ffcdf3ea69a7469dfefc264cf704541bf093ddf9
hengmaozhang <hengmaozhang@users.noreply.github.com>
2025-03-02 03:00:36 +0000

    增加显示上次修改时间的功能

---
+++ assets/chunks/classDiagram-LNE6IOMH.3cfRr2al.js
@@ -0,0 +1,1 @@
+import{s,c as e,a as r,b as t}from"./chunk-T2TOU4HS.TvFtbPND.js";import{_ as l}from"../app.BSmXasT_.js";import"./chunk-5HRBRIJM.DCg7HZUo.js";import"./framework.C9ROEaIA.js";import"./theme.D-X9yb1q.js";var p={parser:t,db:r,renderer:e,styles:s,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute,r.clear()},"init")};export{p as diagram};
